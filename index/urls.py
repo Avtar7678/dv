05903539fd8a4416bc95b64ace5a3a2fe666b71e
Avtar7678 <107638524+Avtar7678@users.noreply.github.com>
2023-02-25 18:30:59 +0530

    Add files via upload

--- index/urls.py
+++ index/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from index import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('index.urls'))
+urlpatterns = [path('', views.home, name='home'),
+path('home', views.home, name='home'),
+path('about', views.about, name='about'),
+path('project', views.project, name='project'),
+ path('contact', views.contact, name='contact')
+         
+   
 ]
