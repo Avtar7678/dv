d75464ff59e68d2bce18ba27ae6016d294e21fd3
Avtar7678 <107638524+Avtar7678@users.noreply.github.com>
2023-02-25 18:35:26 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-)#r4bg_-9q85^2!)bfi9kvflm=jz9sk!f*_8a5is_5yaly%=)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+
 
 
 # Application definition
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
 STATIC_ROOT= os.path.join(BASE_DIR,"staticfiles")
 
 # Default primary key field type
